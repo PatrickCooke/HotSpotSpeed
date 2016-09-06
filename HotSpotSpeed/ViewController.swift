d2d4c0c24dadc12b2e9d58996307e5fcbfe8e7c5
PatrickCooke <cookepa1@gmail.com>
2016-09-06 17:03:21 -0400

    Initial Commit

---
+++ HotSpotSpeed/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HotSpotSpeed
+//
+//  Created by Patrick Cooke on 9/6/16.
+//  Copyright © 2016 Patrick Cooke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
