59c49bdd0c850e47e482580f561a7d4c05464549
PatrickCooke <cookepa1@gmail.com>
2016-09-12 22:02:30 -0400

    set up maps and setting pins from array

---
+++ HotSpotLocation.swift
@@ -0,0 +1,26 @@
+//
+//  HotSpotLocation.swift
+//  HotSpotSpeed
+//
+//  Created by Patrick Cooke on 9/12/16.
+//  Copyright © 2016 Patrick Cooke. All rights reserved.
+//
+
+import Foundation
+import MapKit
+
+class Artwork: NSObject, MKAnnotation {
+    let title: String?
+    let locationName: String
+    let discipline: String
+    let coordinate: CLLocationCoordinate2D
+    
+    init(title: String, locationName: String, discipline: String, coordinate: CLLocationCoordinate2D) {
+        self.title = title
+        self.locationName = locationName
+        self.discipline = discipline
+        self.coordinate = coordinate
+        
+        super.init()
+    }
+}
--- HotSpotSpeed/MapViewController.swift
+++ HotSpotSpeed/MapViewController.swift
@@ -14,26 +14,67 @@ class MapViewController: UIViewController, MKMapViewDelegate {
 
     var locManager = LocationManager.sharedInstance
     var dataManager = DataManager.sharedInstance
+    var wifiArray = [HotSpot]()
     @IBOutlet weak var hsMapView    :   MKMapView!
     
     func setupMap() {
         hsMapView.delegate = self
         hsMapView.showsUserLocation = true
         hsMapView.userTrackingMode = .FollowWithHeading
-        /* - Map Region not working yet
-        let userLoc = hsMapView.userLocation.coordinate
-        let mapSize = MKMapSize(width: 100.0, height: 100.0)
-        let maprect = MKMapRect(origin: (MKMapPointForCoordinate(userLoc)), size: mapSize)
-        hsMapView.mapRectThatFits(maprect)
-        */
-        
+        let regionRadius: CLLocationDistance = 1000
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(hsMapView.userLocation.coordinate,
+                                                                  regionRadius*2.0,
+                                                                  regionRadius*2.0)
+        hsMapView.regionThatFits(coordinateRegion)
     }
     
+    func setArray() {
+        self.wifiArray = self.dataManager.hSArray
+        print("array set with \(self.wifiArray.count) items")
+    }
+    
+    func displayEndpoints(array: [HotSpot]) {
+        let location = MKPointAnnotation()
+        print("displaying endpoints")
+        let count = array.count - 1
+        for index in 0...count {
+            print("endpoint #\(index)")
+            guard let lat = Double(array[index].hpLat) else {
+                return
+            }
+            guard let lon = Double(array[index].hpLon) else {
+                return
+            }
+            guard let title = array[index].hpSSIDName  else {
+                return
+            }
+            guard let downSpeed = array[index].hpDown else {
+                return
+            }
+            guard let upSpeed = array[index].hpUp else {
+                return
+            }
+            let subtitle = "Down: \(downSpeed) Up: \(upSpeed)"
+            let coords = CLLocationCoordinate2DMake(lat, lon)
+            location.coordinate = coords
+            location.title = title
+            location.subtitle = subtitle
+            
+            hsMapView.addAnnotation(location)
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         hsMapView.showsUserLocation = true
+        setArray()
+        setupMap()
         
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        displayEndpoints(self.dataManager.hSArray)
     }
 
     override func didReceiveMemoryWarning() {
