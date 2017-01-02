aa985640e378264143615d56dac20a01efeefda4
Patrick Cooke <cookepa1@gmail.com>
2017-01-02 12:40:09 -0500

    changed min req to iOS9
    also added Options object and removed rotation feature

---
+++ HotSpotSpeed/Options.swift
@@ -0,0 +1,15 @@
+//
+//  Options.swift
+//  HotSpotSpeed
+//
+//  Created by Patrick Cooke on 1/2/17.
+//  Copyright © 2017 Patrick Cooke. All rights reserved.
+//
+
+import Foundation
+
+class Options: NSObject {
+    static let sharedInstance = Options()
+    
+    
+}
